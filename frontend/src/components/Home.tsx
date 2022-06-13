f50212fa98892c6fca6a385a37a97de61a8c9e1f
Linus Romland <romlandlinus@gmail.com>
2022-06-13 18:24:58 +0200

    Formatted project with prettier config

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -1,10 +1,20 @@
 const Home = () => (
-  <div className="home">
-    <div className="home-message">
-      <h3>Välkommen </h3>
-      <p >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mattis nunc sed blandit libero. Arcu ac tortor dignissim convallis aenean et tortor. Commodo sed egestas egestas fringilla phasellus faucibus scelerisque eleifend donec. Eget egestas purus viverra accumsan in nisl nisi. Aliquet enim tortor at auctor urna nunc id cursus metus. Penatibus et magnis dis parturient montes nascetur. Vitae aliquet nec ullamcorper sit amet. Vitae sapien pellentesque habitant morbi tristique senectus et netus et. Eu facilisis sed odio morbi quis commodo odio aenean. Hendrerit gravida rutrum quisque non. Dui vivamus arcu felis bibendum ut tristique et. Lorem mollis aliquam ut porttitor leo a diam. Morbi non arcu risus quis varius quam quisque.</p>
+    <div className='home'>
+        <div className='home-message'>
+            <h3>Välkommen </h3>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                incididunt ut labore et dolore magna aliqua. Mattis nunc sed blandit libero. Arcu ac
+                tortor dignissim convallis aenean et tortor. Commodo sed egestas egestas fringilla
+                phasellus faucibus scelerisque eleifend donec. Eget egestas purus viverra accumsan
+                in nisl nisi. Aliquet enim tortor at auctor urna nunc id cursus metus. Penatibus et
+                magnis dis parturient montes nascetur. Vitae aliquet nec ullamcorper sit amet. Vitae
+                sapien pellentesque habitant morbi tristique senectus et netus et. Eu facilisis sed
+                odio morbi quis commodo odio aenean. Hendrerit gravida rutrum quisque non. Dui
+                vivamus arcu felis bibendum ut tristique et. Lorem mollis aliquam ut porttitor leo a
+                diam. Morbi non arcu risus quis varius quam quisque.
+            </p>
+        </div>
     </div>
-
-  </div>
-)
-export default Home
+);
+export default Home;
