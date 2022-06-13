733ea1b61687cb23f4368da095b1158693dbf649
Linus Romland <hello@linusromland.com>
2022-06-13 18:36:27 +0200

    #22 initera eslint prettier på frontend (#23)
    * Fix format in `App.tsx` for router
    
    * Add ESLint and Prettier
    
    * Fix ESLint errors
    
    * Add workflows for ESLint & Prettier
    
    * Fix error in workflow
    
    * Formatted project with prettier config
    
    * Update frontendPrettier.yaml

--- frontend/src/Components/SimpleGetRequestTemplate.tsx
+++ frontend/src/Components/SimpleGetRequestTemplate.tsx
@@ -1,25 +1,25 @@
-import React, { FC, useState } from "react";
-import axios from "axios";
+import React, { FC, useState } from 'react';
+import axios from 'axios';
 
 const GetTemplate: FC = () => {
-  const [responses, setResponses] = useState<string[]>([]);
-  const get = async () => {
-    const res = await axios.get("https://localhost:7156/Template");
-    const data = res.data;
-    console.log(data);
-    setResponses((u) => (u = data));
-  };
+    const [responses, setResponses] = useState<string[]>([]);
+    const get = async () => {
+        const res = await axios.get('https://localhost:7156/Template');
+        const data = res.data;
+        console.log(data);
+        setResponses((u) => (u = data));
+    };
 
-  return (
-    <div className="GetTemplate">
-      <button onClick={get}>click get</button>
-      <ul>
-        {responses.map((txt) => (
-          <li>{txt}</li>
-        ))}
-      </ul>
-    </div>
-  );
+    return (
+        <div className='GetTemplate'>
+            <button onClick={get}>click get</button>
+            <ul>
+                {responses.map((txt, index) => (
+                    <li key={index}>{txt}</li>
+                ))}
+            </ul>
+        </div>
+    );
 };
 
 export default GetTemplate;
