7f0d68b84b7fa836c97531d3a14655a60fcd90e1
Linus Romland <romlandlinus@gmail.com>
2022-06-14 11:02:44 +0200

    Disabled eslint check for createRoot

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <BrowserRouter>
